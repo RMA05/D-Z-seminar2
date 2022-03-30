10baaa20b7e519b7d1555b26219c36190466c3d5
Murad <11_rma@mail.ru>
2022-03-30 14:41:04 +0300

    seminar2

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿// Задача 13.
+
+int numberA = Convert.ToInt32(Console.ReadLine());
+int numberB = Convert.ToInt32(Console.ReadLine());
+if (numberA % numberB == 0)
+{
+     System.Console.WriteLine("Число кратное ");
+}
+else
+{
+    System.Console.WriteLine("Число  не кратное ");
+}
